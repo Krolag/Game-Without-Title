c86151ece7e3373983e10bbbf9737046e5481ca0
Ignole <ignole@icloud.com>
2020-11-08 21:51:12 +0100

    Pistol model has been added as well as simple shooting system (projectiles).

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public GameObject bullet;
+
+    public float speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+            Shot();
+    }
+
+    private void Shot()
+    {
+        GameObject cB = Instantiate(bullet, spawnPoint.position, bullet.transform.rotation);
+        Rigidbody rig = cB.GetComponent<Rigidbody>();
+        
+        rig.AddForce(spawnPoint.forward * speed, ForceMode.Impulse);
+    }
+}
