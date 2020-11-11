41e3705d2267249e8302f6008022e0333997c2a5
224407@edu.p.lodz.pl <kub@2PDH>
2020-11-11 19:29:13 +0100

    Added door animations, all windows, all doors, simple table and locker

---
+++ Assets/Scripts/Level/TriggerDoorController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    public Animator myDoor = null;
+
+    public bool doorTrigger = false;
+    public bool isDoorOpen = false;
+    
+    private void OnTriggerStay(Collider other)
+    { 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (other.CompareTag("Player"))
+            {
+
+                if (doorTrigger)
+                {
+                    if (!isDoorOpen)
+                    {
+                        myDoor.Play("openDoor", 0, 0.0f);
+                        isDoorOpen = true;
+                    }
+                    else if (isDoorOpen)
+                    {
+                        myDoor.Play("closeDoor", 0, 0.0f);
+                        isDoorOpen = false;
+                    }
+                }
+            }
+        }
+    }
+}
