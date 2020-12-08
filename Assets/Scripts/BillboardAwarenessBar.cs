10fe61c2675bfb0bc6cdd89973183db92a7ed06e
Krolag <dawidpaweloszek99@gmail.com>
2020-12-08 22:04:05 +0100

    fixed billboard bar

--- Assets/Scripts/BillboardAwarenessBar.cs
+++ Assets/Scripts/BillboardAwarenessBar.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BillboardAwarenessBar : MonoBehaviour
 {
     public Transform PlayerCamera;
+
+    private void Start()
+    {
+        PlayerCamera = Camera.main.transform;
+    }
 
     // LateUpdate is called after Update function
     void LateUpdate()
