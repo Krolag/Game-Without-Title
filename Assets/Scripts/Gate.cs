356c2a3d8d56a7bbbe52018ea585c24ae22a6525
LinearJester <linearjester@gmail.com>
2021-01-11 20:14:05 +0100

    added gate opening

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] private Transform _openPosition;
+    [SerializeField] private Transform _closePosition;
+    [SerializeField] private float _step;
+
+    private Coroutine _currentlyUsedCoroutine;
+
+    [ContextMenu("open")]
+    public void Open()
+    {
+        if (gameObject.transform.position == _openPosition.position)
+            return;
+        if (_currentlyUsedCoroutine != null)
+            StopCoroutine(_currentlyUsedCoroutine);
+
+        _currentlyUsedCoroutine = StartCoroutine(Move(_openPosition.position, _step));
+    }
+    [ContextMenu("close")]
+    public void Close()
+    {
+        if (gameObject.transform.position == _closePosition.position)
+            return;
+        if (_currentlyUsedCoroutine != null)
+            StopCoroutine(_currentlyUsedCoroutine);
+
+        _currentlyUsedCoroutine = StartCoroutine(Move(_closePosition.position, _step));
+    }
+
+    private IEnumerator Move(Vector3 position, float step)
+    {
+        while (gameObject.transform.position != position)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, position, step * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Activatable))]
+public class Lever : MonoBehaviour
+{
+    [SerializeField] private Gate _gate;
+    private bool _opened;
+
+    private void Awake()
+    {
+        _opened = false;
+    }
+
+    private void OnEnable()
+    {
+        GetComponent<Activatable>().Activated += OnActivated;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<Activatable>().Activated -= OnActivated;
+    }
+
+    public void OnActivated()
+    {
+        if (_opened)
+        {
+            _gate.Close();
+            _opened = false;
+        }
+        else
+        {
+            _gate.Open();
+            _opened = true;
+        }
+    }
+
+}
