b5af87e56e398e37eac7e73694b94cbd327409f3
Krolag <dawidpaweloszek99@gmail.com>
2021-01-13 21:03:28 +0100

    Working on changing levels

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+    [SerializeField] private Transform _entryPoint;
+    [SerializeField] private Transform _exitPoint;
+    [SerializeField] private LevelManager _levelManager;
+
+    public Transform EntryPoint => _entryPoint;
+    public Transform ExitPoint => _exitPoint;
+
+    private void Update()
+    {
+        if (Vector3.Distance(_levelManager.Player.transform.position, _exitPoint.position) < 5)
+            _levelManager.NextLevel();
+    }
+}
--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,17 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject[] _levelPrefabs;
+    [SerializeField] private int _levelIndex = 0;
+
+    public GameObject Player => _player;
+
+    private void Start()
     {
-        
+        if (_levelIndex == 0)
+        {
+            _levelPrefabs[_levelIndex].SetActive(true);
+            _player.transform.position = _levelPrefabs[_levelIndex].GetComponent<LevelChanger>().EntryPoint.transform.position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextLevel()
+    {
+        _levelPrefabs[_levelIndex].SetActive(false);
+        _levelIndex++;
+        _levelPrefabs[_levelIndex].SetActive(true);
+        _player.transform.position = _levelPrefabs[_levelIndex].GetComponent<LevelChanger>().EntryPoint.transform.position;
+    }
+
+    public void RestartLevel()
     {
         
     }
