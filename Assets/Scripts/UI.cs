9dce33b0d1bc5de838096a76ef8a78be58f9cef9
Dzalapino <224407@edu.p.lodz.pl>
2021-01-26 02:58:24 +0100

    New main menu with in game screen

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    public GameObject _backButton;
+    public GameObject _quitButton;
+    public GameObject _playButton;
+    public GameObject _authorsButton;
+    public GameObject _authorsPanel;
+    public GameObject _descriptionText;
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
@@ -15,5 +22,29 @@ public class UI : MonoBehaviour
     public void LoadFirstLevel()
     {
         SceneManager.LoadScene("newLevel_00", LoadSceneMode.Single);
+    }
+    public void OnBack()
+    {
+        _backButton.SetActive(false);
+        _authorsPanel.SetActive(false);
+        _quitButton.SetActive(true);
+        _playButton.SetActive(true);
+        _authorsButton.SetActive(true);
+        _descriptionText.SetActive(true);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    public void OnAuthors()
+    {
+        _backButton.SetActive(true);
+        _authorsPanel.SetActive(true);
+        _quitButton.SetActive(false);
+        _playButton.SetActive(false);
+        _authorsButton.SetActive(false);
+        _descriptionText.SetActive(false);
     }
 }
